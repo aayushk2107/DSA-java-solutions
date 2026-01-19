b6afe611c6374bdfd8b06fdd5543bd3d59fdaec6
Aayush kumar <aayushkumar21072107@gmail.com>
2026-01-19 13:22:30 +0530

    Solved Leetcode how many numbers are smaller than the current number

---
+++ 01-Arrays/LC8_NoSmallThanCurrentNo.java
@@ -0,0 +1,22 @@
+//Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+//Return the answer in an array.
+
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        if(nums.length==0){
+            return new int []{-1};
+        }
+        int []ans=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            int num=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]>nums[j]){
+                    num++;
+                    ans[i]=num;
+                }
+            }
+        }
+        return ans;
+    }
+}
