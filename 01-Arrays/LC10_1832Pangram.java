23b13aa575704de6372d44e6f7889459acbef9eb
Aayush kumar <aayushkumar21072107@gmail.com>
2026-01-20 21:13:47 +0530

    Checking pangram

---
+++ 01-Arrays/LC10_1832Pangram.java
@@ -0,0 +1,14 @@
+/*A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.*/
+
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        for (char i = 'a'; i <= 'z'; i++) {
+            if (sentence.indexOf(i) == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
