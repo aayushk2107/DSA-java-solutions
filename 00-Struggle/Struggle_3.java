1c7144eeadf8bae49d3731b3d56506059a0a86ae
Aayush kumar <aayushkumar21072107@gmail.com>
2026-01-29 19:09:31 +0530

    Mistakes i did almost 4 to 5 times

---
+++ 00-Struggle/Struggle_3.java
@@ -0,0 +1,11 @@
+/*Learning a lot from the mistakes :
+
+1. Tried to reappear to old questions and got stuck up first and then actually solves it
+
+2.Tried to force doing the questions after recognizing the pattern easily which made 
+ me stuck and forget the question in my next time of attempt 
+
+3. Stucking and applying another methods to my questions in my second visit tells me 
+ that i need to create an example on my own to properly understand the question and
+ commit a blunder like applyig another methods damn it
+*/
