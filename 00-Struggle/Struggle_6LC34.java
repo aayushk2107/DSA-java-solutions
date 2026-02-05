7b3dd786be9517a2366c72005bdb045cfbbaf5d0
aayushk2107 <aayushkumar21072107@gmail.com>
2026-02-05 20:43:40 +0530

    Update Struggle_6LC34.java

--- 00-Struggle/Struggle_6LC34.java
+++ 00-Struggle/Struggle_6LC34.java
@@ -4,13 +4,10 @@ Did wrong code and it ran even on leetcode but after submitting it i realised th
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         ArrayList<Integer> a1=new ArrayList<>();
-        int j=0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==target && j=0){
+            if(nums[i]==target){
                 a1.add(i);
-                j++;
             }
-            else if()
         }
         if(a1.size()==0){
             return new int[]{-1,-1};
