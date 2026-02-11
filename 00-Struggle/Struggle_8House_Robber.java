e019a4c0a5657addcf65b4c7faa9c8ff8c9aeebe
Aayush kumar <aayushkumar21072107@gmail.com>
2026-02-11 22:09:02 +0530

    House Robber Problem I Got

---
+++ 00-Struggle/Struggle_8House_Robber.java
@@ -0,0 +1,13 @@
+/* 
+ Struggled in Bank robber problem of leetcode:
+ 
+ 1. Got wrong in understanding the problem statement.
+ 2. Got wrong in classifying the problem correctly.
+ 3. Got wrong in understanding the goal is globally.
+    optimal sum of the money in the house and the rule
+    is like to not rob in adjacent houses.
+    for  example: [100,1,1,100]
+     answer should be 200 skip index 1 and 2 to get the optimal
+     answer if i include 1 i have to skip 100 and that breaks the goal
+*/
+   
