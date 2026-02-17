832f0e1fed73021d0931e959799daa98c23b4efb
Aayush kumar <aayushkumar21072107@gmail.com>
2026-02-17 22:01:38 +0530

    Dry ran BS questions

---
+++ 00-Struggle/Struggle_9BS.java
@@ -0,0 +1,8 @@
+Practised Binary Search by dry running 10 times on paper
+Understood things like 
+Binary search always found the boundary dictated by a Boolean condition
+_ and that Boolean condition adapts to the problem shaping how the search space shrinks 
+
+
+found out one cool analogy from binary search even 
+ 
