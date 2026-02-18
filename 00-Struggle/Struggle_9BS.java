f7df5126aae0e501ece6fc0fd70bb5db35787f63
aayushk2107 <aayushkumar21072107@gmail.com>
2026-02-18 21:23:50 +0530

    Update Struggle_9BS.java

--- 00-Struggle/Struggle_9BS.java
+++ 00-Struggle/Struggle_9BS.java
@@ -2,7 +2,10 @@ Practised Binary Search by dry running 10 times on paper
 Understood things like 
 Binary search always found the boundary dictated by a Boolean condition
 _ and that Boolean condition adapts to the problem shaping how the search space shrinks 
-
+Now in my words
+  the boolean condition adapts to the condition like whether it is a condition if it is 
+  greater than or equal to less than or equal to just greater than or just equal to or just
+  anything
 
 found out one cool analogy from binary search even 
  
