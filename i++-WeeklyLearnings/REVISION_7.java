0f0cf95e1c02544051942542f4852c4ced96cf78
aayushk2107 <aayushkumar21072107@gmail.com>
2026-03-05 22:15:09 +0530

    Revise ceiling question to count even digits

--- i++-WeeklyLearnings/REVISION_7.java
+++ i++-WeeklyLearnings/REVISION_7.java
@@ -1,51 +1,26 @@
-//revised ceiling question
+//revised even count leetcode question
 public class Main{
     public static void main(String[]args){
-        int []arr={1,2,3,5,7,11};
-        int ans=ceiling(arr,5);
+        int []arr={1,2,3,34,56,9};
+        int ans=numberCount(arr);
         System.out.println(ans);
     }
-    static int ceiling(int []nums,int target){
+    static int numberCount(int []nums){
         if(nums.length==0){
             return -1;
         }
-        int start=0;
-        int end=nums.length-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]>target){
-                end=mid-1;
+        int evencount=0;
+        for(int i=0;i<nums.length;i++){
+            int digitcount=0;
+            int element=nums[i];
+            while(element>0){
+                element=element/10;
+                digitcount++;
             }
-            else{
-                start=mid+1;
-            }
-        }
-        return start;
-    }
-}
-
-//Editing cause attempted kadane on a self generaed array and got a little tweaky answer on 28/02/2026
-
-    public class Main{
-    public static void main(String[]args){
-        int []arr={10,20,15,16,17,22};
-        int ans=kadane(arr);
-        System.out.println(ans);
-    }
-    static int kadane(int []nums){
-        int currentsum=nums[0];
-        int maxsum=nums[0];
-        for(int i=1;i<nums.length;i++){
-            if(currentsum+nums[i]>nums[i]){
-                currentsum=currentsum+nums[i];
-            }
-            else{
-                currentsum=nums[i];
-            }
-            if(currentsum>maxsum){
-                maxsum=currentsum;
+            if(digitcount%2==0){
+                evencount++;
             }
         }
-        return maxsum;
+        return evencount;
     }
 }
