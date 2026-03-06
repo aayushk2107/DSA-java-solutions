bfd30c016e2328573adaa6f1d61f8e9b40d9eba1
aayushk2107 <aayushkumar21072107@gmail.com>
2026-03-06 23:16:06 +0530

    Revise running sum method and input array

--- i++-WeeklyLearnings/REVISION_8.java
+++ i++-WeeklyLearnings/REVISION_8.java
@@ -1,15 +1,17 @@
+//revised Running sum
 public class Main{
     public static void main(String[]args){
-        int []arr={1,2,34,23};
-        int ans=runningEvenSum(arr);
+        int []arr={1,2,3,4,5};
+        int ans=runningSum(arr);
         System.out.println(ans);
     }
-    static int runningEvenSum(int []nums){
+    static int runningSum(int[]nums){
+        if(nums.length==0){
+            return -1;
+        }
         int sum=0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]%2==0){
-                sum=sum+nums[i];
-            }
+            sum=sum+nums[i];
         }
         return sum;
     }
