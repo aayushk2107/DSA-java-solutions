df6c1c16e5dc9787631828588dc1b99443b0f624
aayushk2107 <aayushkumar21072107@gmail.com>
2026-03-17 21:10:09 +0530

    Refactor main method and implement binary search
    Refactored the main method to use binary search instead of magic23 function. Updated the bS function to implement binary search logic.

--- i++-WeeklyLearnings/REVISION_8.java
+++ i++-WeeklyLearnings/REVISION_8.java
@@ -1,21 +1,27 @@
-import java.util.Arrays;
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[]args){
         int []arr={1,2,3,4,5,6,7};
-        int ans=magic23(arr);
+        int ans=bS(arr,4);
         System.out.println(ans);
     }
-    static int magic23(int []nums){
+    static int bS(int [] nums,int target){
         if(nums.length==0){
             return -1;
         }
-        int sum=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]%nums[i]== 0 && nums[i]%1==0){
-                nums[i]=0;
-                sum++;
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
             }
         }
-        return sum;
+        return -1;
     }
 }
