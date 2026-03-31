03bf358a474fd157d112c1a2c8fdbc871873279d
aayushk2107 <aayushkumar21072107@gmail.com>
2026-03-31 14:37:36 +0530

    Fix binary search to return index of target
    Refactor binary search logic to return index when target is found.

--- 03-Binary-Search/LC0_BinarySearch.java
+++ 03-Binary-Search/LC0_BinarySearch.java
@@ -23,14 +23,14 @@ class Solution {
         int end=nums.length-1;
         while(start<=end){
             int mid=start+(end-start)/2;
-            if(nums[mid]<target){
-                start=mid+1;
+            if(nums[mid]==target){
+                return mid;
             }
             else if(nums[mid]>target){
                 end=mid-1;
             }
             else{
-                return mid;
+                start=mid+1;
             }
         }
         return -1;
