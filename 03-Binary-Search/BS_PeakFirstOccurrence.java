fc32ee929cfcdf071ff492e29ae0d2b503b1c200
aayushk2107 <aayushkumar21072107@gmail.com>
2026-04-08 23:15:37 +0530

    Refactor peak finding and first occurrence methods

--- 03-Binary-Search/BS_PeakFirstOccurrence.java
+++ 03-Binary-Search/BS_PeakFirstOccurrence.java
@@ -1,26 +1,29 @@
 public class PeakFirstOccurrence {
 
+
     static int findPeak(int[] nums) {
         int start = 0;
         int end = nums.length - 1;
 
-        while (start<end) {
-            int mid =start + (end -start) / 2;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
 
-            if (nums[mid] > nums[mid +1]) {
-                end = mid;  
-            } else if (nums[mid] < nums[mid +1]) {
+            if (nums[mid] > nums[mid + 1]) {
+                end = mid;
+            } else if (nums[mid] < nums[mid + 1]) {
                 start = mid + 1;
             } else {
-                end--; 
+                end--;
             }
         }
 
         return start;
     }
 
+   
+    static int firstOccurrenceInPeak(int[] nums, int target) {
 
-    static int firstOccurrenceInPeak(int[] nums, int target, int peakIndex) {
+        int peakIndex = findPeak(nums);  
 
         int start = 0;
         int end = peakIndex;
@@ -29,7 +32,7 @@ public class PeakFirstOccurrence {
             int mid = start + (end - start) / 2;
 
             if (nums[mid] >= target) {
-                end = mid - 1;  
+                end = mid - 1;
             } else {
                 start = mid + 1;
             }
@@ -43,18 +46,12 @@ public class PeakFirstOccurrence {
     }
 
 
-    static int solve(int[] nums, int target) {
-        int peakIndex = findPeak(nums);
-        return firstOccurrenceInPeak(nums, target, peakIndex);
-    }
-
-
     public static void main(String[] args) {
 
         int[] nums = {1, 2, 4, 4, 4, 3, 2};
         int target = 4;
 
-        int ans = solve(nums, target);
+        int ans = firstOccurrenceInPeak(nums, target);
 
         System.out.println(ans);
     }
