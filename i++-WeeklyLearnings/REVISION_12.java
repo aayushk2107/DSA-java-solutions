8e189f12c9b7398f85e6e949a52dbd9b9d1db380
aayushk2107 <aayushkumar21072107@gmail.com>
2026-05-12 20:27:34 +0530

    Update REVISION_12.java

--- i++-WeeklyLearnings/REVISION_12.java
+++ i++-WeeklyLearnings/REVISION_12.java
@@ -23,3 +23,31 @@ public class Main{
         return nums[start];
     }
 }
+on 12th may i solved this
+
+public class Main{
+    public static void main(String[]args){
+        int []arr={5,5,5,6,7,2,3,4};
+        System.out.println(minInRotatedSortedArray(arr));
+    }
+    static int minInRotatedSortedArray(int []nums){
+        if(nums.length==0){
+            return -1;
+        }
+        int start=0;
+        int end=nums.length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==nums[end]){
+                end--;
+            }
+            else if(nums[mid]<nums[end]){
+                end=mid;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return nums[end];
+    }
+}
