d0ee12e9608496eb03cb228ca62f459141f5cbb6
Aayush Kumar <aayushkumar21072107@gmail.com>
2026-05-14 20:54:52 +0530

    Implement binary search in REVISION_12.java

--- i++-WeeklyLearnings/REVISION_12.java
+++ i++-WeeklyLearnings/REVISION_12.java
@@ -105,3 +105,32 @@ public class Main{
         return -1;
     }
 }
+
+on 14th may i solved this
+
+    class Main {
+    public static void main(String[] args) {
+        int []arr={1,2,3,4,5};
+        System.out.println(bS(arr,3));
+    }
+    static int bS(int []nums,int target){
+        if(nums.length==0){
+            return -1;
+        }
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+}
