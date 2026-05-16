23dfdec1c895d1459d9441268e213ef4ff8d8aac
Aayush Kumar <aayushkumar21072107@gmail.com>
2026-05-16 22:01:02 +0530

    Update REVISION_12.java

--- i++-WeeklyLearnings/REVISION_12.java
+++ i++-WeeklyLearnings/REVISION_12.java
@@ -159,3 +159,41 @@ on 15th may i solved this
         return start;
     }
 }
+
+on 16th may i solved this
+    public class Main{
+    public static void main(String[]args){
+        int []arr={7,8,9,1,2,3,4,5,6};
+        System.out.println(rotatedSortedArray(arr,4));
+    }
+    static int rotatedSortedArray(int []nums,int target){
+        if(nums.length==0){
+            return -1;
+        }
+        int start=0;
+        int end=nums.length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<=start){
+                if(nums[start]<nums[mid] && nums[mid]>target){
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<target && nums[end]>target){
+                    start=mid+1;
+                }
+                else{
+                    end=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
