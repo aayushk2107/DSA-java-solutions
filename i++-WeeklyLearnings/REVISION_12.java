0cff85c68b7452434ca6ce892651f62dfbb62007
Aayush Kumar <aayushkumar21072107@gmail.com>
2026-05-20 21:22:41 +0530

    Fix import statement formatting in REVISION_12.java

--- i++-WeeklyLearnings/REVISION_12.java
+++ i++-WeeklyLearnings/REVISION_12.java
@@ -286,8 +286,8 @@ public class Main{
 }
 
 on 20th may i solved this
-
-    import java.util.Arrays;
+    
+import java.util.Arrays;
 public class Main{
     public static void main(String[]args){
         int []arr={1,2,3,4,5};
