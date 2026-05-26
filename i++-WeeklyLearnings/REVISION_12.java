c835fae104a3431a1a5d9789a6a718b604573b57
Aayush Kumar <254232086+aayushk2107@users.noreply.github.com>
2026-05-26 21:22:53 +0530

    Implement binary search in rotated sorted array

--- i++-WeeklyLearnings/REVISION_12.java
+++ i++-WeeklyLearnings/REVISION_12.java
@@ -424,3 +424,42 @@ on 25th may i did this simple shit
         return sum;
     }
 }
+
+on 26th may i did this
+
+    public class Main{
+    public static void main(String[]args){
+        int []arr={5,6,7,1,2,3,4};
+        System.out.println(rotatedSorted(arr,6));
+    }
+    static int rotatedSorted(int []nums,int target){
+        if(nums.length==0){
+            return -1;
+        }
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                return nums[mid];
+            }
+            else if(nums[mid]>=nums[start]){
+                if(nums[mid]>target && nums[mid]<=nums[start]){
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<target && nums[end]>=target){
+                    start=mid+1;
+                }
+                else{
+                    end=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
