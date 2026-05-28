a29ca707f383b698eae7e4018e8f02b21ddd68df
Aayush Kumar <254232086+aayushk2107@users.noreply.github.com>
2026-05-28 15:23:01 +0530

    Add solution for Koko eating bananas problem
    Implemented binary search for minimum eating speed in Koko's banana problem.

--- 00-Struggle/Struggle_10BS.java
+++ 00-Struggle/Struggle_10BS.java
@@ -47,3 +47,34 @@ class Solution {
         return end;
     }
 }
+
+my wrong attempt on koko eating bananas on 28th may
+
+    
+    class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+         int start=1;
+         int end=piles[0];
+         for(int i=1;i<piles.length;i++){
+            if(end<piles[i]){
+                end=piles[i];
+         }
+         while(start<end){
+            int mid=start+(end-start)/2;
+            if(hoursEstimation(piles,h,mid)){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+         }
+         return start;
+    }
+    public boolean hoursE(int []piles,int h,int k){
+        long hours=0;
+        for(int i=0;i<piles.length;i++){
+            hours=hours+(piles[i]+k-1)/k;
+        }
+        return hours<=h;
+    }
+}
