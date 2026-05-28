5f26a28dd23c867fa5f0639bec28f279e35b6fa5
Aayush kumar <aayushkumar21072107@gmail.com>
2026-05-28 15:20:57 +0530

    Solved koko eating bananas

---
+++ 03-Binary-Search/LC15_KokoEatingBananas.java
@@ -0,0 +1,53 @@
+/*
+Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+Return the minimum integer k such that she can eat all the bananas within h hours.
+
+ 
+
+Example 1:
+
+Input: piles = [3,6,7,11], h = 8
+Output: 4
+Example 2:
+
+Input: piles = [30,11,23,4,20], h = 5
+Output: 30
+Example 3:
+
+Input: piles = [30,11,23,4,20], h = 6
+Output: 23
+*/
+
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+         int start=1;
+         int end=piles[0];
+         for(int i=1;i<piles.length;i++){
+            if(end<piles[i]){
+                end=piles[i];
+            }
+         }
+         while(start<=end){
+            int mid=start+(end-start)/2;
+            if(hoursEstimation(piles,h,mid)){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+         }
+         return start;
+    }
+    public boolean hoursEstimation(int []piles,int h,int k){
+        long hours=0;
+        for(int i=0;i<piles.length;i++){
+            hours=hours+(piles[i]+k-1)/k;
+        }
+        return hours<=h;
+    }
+}
