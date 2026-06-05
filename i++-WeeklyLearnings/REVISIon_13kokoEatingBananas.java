c735cdc9ae371f52b48d6504a2fb8b5be6080b05
Aayush Kumar <254232086+aayushk2107@users.noreply.github.com>
2026-06-05 21:05:00 +0530

    Update REVISIon_13kokoEatingBananas.java

--- i++-WeeklyLearnings/REVISIon_13kokoEatingBananas.java
+++ i++-WeeklyLearnings/REVISIon_13kokoEatingBananas.java
@@ -36,3 +36,42 @@ public class Main{
         return hour<=h;
     }
 }
+
+on 5th june i did this
+
+    class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int start=1;
+        int end=piles[0];
+        for(int i=0;i<piles.length;i++){
+            if(piles[i]>end){
+                end=piles[i];
+            }
+        }
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(kSpeed(piles,h,mid)){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return start;
+    }
+    public boolean kSpeed(int []nums,int h,int k){
+        long hours=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] % k == 0){
+                hours=hours+nums[i]/k;
+            }
+            else if(nums[i]>k){
+                hours=hours+nums[i]/k+1;
+            }
+            else{
+                hours=hours+1;
+            }
+        }
+        return hours<=h;
+    }
+}
