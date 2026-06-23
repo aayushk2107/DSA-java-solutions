8cff8dc7323ef15dc42b5ac61db23604ab092928
Aayush kumar <aayushk2107@users.noreply.github.com>
2026-06-23 19:48:15 +0530

    Solved slight modification of the same pattern of prefix sum+hashing

---
+++ 04-Hashing/LC15_ContinuousSubarraySum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        map.put(0,-1);
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+            int remainder=sum%k;
+            if(map.containsKey(remainder)==true){
+                int requiredIndex=i-map.get(remainder);
+                if(requiredIndex>=2){
+                    return true;
+                }
+            }
+            if(!map.containsKey(remainder)){
+                map.put(remainder,i);
+            }
+        }
+        return false;
+    }
+}
