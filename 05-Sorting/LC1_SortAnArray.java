b737978ee0c9a11416ecf8f4d9ad3439d665989d
Aayush kumar <aayushk2107@users.noreply.github.com>
2026-07-01 21:06:10 +0530

    Not done yet

---
+++ 05-Sorting/LC1_SortAnArray.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+}
+
+//WILL RETURN AFTER RECURSION TO SOLVE THE SAME USING MERGE OR QUICK SORT
