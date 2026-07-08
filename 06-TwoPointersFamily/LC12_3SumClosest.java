e403fa4b9e2ebe84d001c6d370aea0f9383cba2b
Aayush Kumar <254232086+aayushk2107@users.noreply.github.com>
2026-07-08 13:14:35 +0530

    Refactor threeSumClosest method for clarity

--- 06-TwoPointersFamily/LC12_3SumClosest.java
+++ 06-TwoPointersFamily/LC12_3SumClosest.java
@@ -1,1 +1,44 @@
-class Solution {public int threeSumClosest(int[] nums, int target) {Arrays.sort(nums);int globalDistance=Integer.MAX_VALUE;int sum=0;if(nums.length==3){sum=nums[0]+nums[1]+nums[2];return sum;}for(int i=0;i<nums.length-2;i++){if(i>=1){if(nums[i]==nums[i-1]){continue;}}int start=i+1;int end=nums.length-1;while(start<end){int currentSum=nums[i]+nums[start]+nums[end];if(currentSum<target){start++;while(start<end && nums[start]==nums[start-1]){start++;}}else if(currentSum>target){end--;while(start<end && nums[end]==nums[end+1]){end--;}}else{return currentSum;}int currentDistance=Math.abs(currentSum-target);if(currentDistance<globalDistance){globalDistance=currentDistance;sum=currentSum;}}}return sum;}}
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int globalDistance=Integer.MAX_VALUE;
+        int sum=0;
+        if(nums.length==3){
+            sum=nums[0]+nums[1]+nums[2];
+            return sum;
+        }
+        for(int i=0;i<nums.length-2;i++){
+            if(i>=1){
+                if(nums[i]==nums[i-1]){
+                    continue;
+                }
+            }
+            int start=i+1;
+            int end=nums.length-1; 
+            while(start<end){
+                int currentSum=nums[i]+nums[start]+nums[end];
+                if(currentSum<target){
+                    start++;
+                    while(start<end && nums[start]==nums[start-1]){
+                        start++;
+                    }
+                }
+                else if(currentSum>target){
+                    end--;
+                    while(start<end && nums[end]==nums[end+1]){
+                        end--;
+                    }
+                }
+                else{
+                    return currentSum;
+                }
+                int currentDistance=Math.abs(currentSum-target);
+                if(currentDistance<globalDistance){
+                    globalDistance=currentDistance;
+                    sum=currentSum;
+                }
+            }
+        }
+        return sum;
+    }
+}
