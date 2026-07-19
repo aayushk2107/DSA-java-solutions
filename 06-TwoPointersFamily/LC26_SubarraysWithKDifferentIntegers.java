921933cc624b1fc86212ef6f4082cf69f41bcabc
Aayush Kumar <254232086+aayushk2107@users.noreply.github.com>
2026-07-19 18:01:50 +0530

    Add optimal solution for subarrays with K distinct integers
    Implemented an optimal solution to count subarrays with exactly K distinct integers using the concept of at most K and at most K-1.

--- 06-TwoPointersFamily/LC26_SubarraysWithKDifferentIntegers.java
+++ 06-TwoPointersFamily/LC26_SubarraysWithKDifferentIntegers.java
@@ -23,3 +23,40 @@ class Solution {
         return validSubarrays;
     }
 }
+
+
+OPTIMAL SOLUTION USING ATMOST(K) - ATMOST(K-1) 
+
+    class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        int atMostK=0;
+        int left=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int right=0;right<nums.length;right++){
+            map.put(nums[right],map.getOrDefault(nums[right],0)+1);
+            while(map.size()>k){
+                map.put(nums[left],map.get(nums[left])-1);
+                if(map.get(nums[left])==0){
+                    map.remove(nums[left]);
+                }
+                left++;
+            }
+            atMostK+=right-left+1;
+        }
+        int left1=0;
+        int atMostK1=0;
+        HashMap<Integer,Integer> map2=new HashMap<>();
+        for(int right=0;right<nums.length;right++){
+            map2.put(nums[right],map2.getOrDefault(nums[right],0)+1);
+            while(map2.size()>k-1){
+                map2.put(nums[left1],map2.get(nums[left1])-1);
+                if(map2.get(nums[left1])==0){
+                    map2.remove(nums[left1]);
+                }
+                left1++;
+            }
+            atMostK1+=right-left1+1;
+        }
+        return atMostK-atMostK1;
+    }
+}
