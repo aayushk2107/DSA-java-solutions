f0272ec894953a23bfc700281ec6b0a92d5fc571
Aayush Kumar <254232086+aayushk2107@users.noreply.github.com>
2026-07-23 12:06:55 +0530

    Add binary search and sliding window for subarray sum
    Implemented a binary search approach combined with a fixed size sliding window to find the minimum size subarray sum.

--- 06-TwoPointersFamily/LC18_MinimumSizeSubarraySum.java
+++ 06-TwoPointersFamily/LC18_MinimumSizeSubarraySum.java
@@ -24,3 +24,47 @@ class Solution {
         return globalLength;
     }
 }
+
+AAPROACH 2
+    
+    USING BS ON ANSWER AND FIXED SIZE SLIDING WINDOW 
+
+    
+    class Solution {
+    public int minSubArrayLen(int target,int[] nums) {
+        int start=0;
+        int end=nums.length;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(validator(mid,target,nums)){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        if(start>nums.length){
+            return 0;
+        }
+        return start;
+    }
+
+    public boolean validator(int mid,int tar,int[] arr){
+        int sum=0;
+        int left=0;
+        for(int i=0;i<arr.length;i++){
+            if(i<=mid-1){
+                sum+=arr[i];
+            }
+            else{
+                sum+=arr[i];
+                sum-=arr[left];
+                left++;
+            }
+            if(sum>=tar){
+                return true;
+            }
+        }
+        return false;
+    }
+}
