5c396b6882147e5605350eff0080be74a3c84366
Aayush Kumar <254232086+aayushk2107@users.noreply.github.com>
2026-07-28 19:28:56 +0530

    Update LC6_LinkedListCycle.java

--- 07-LinkedList/LC6_LinkedListCycle.java
+++ 07-LinkedList/LC6_LinkedListCycle.java
@@ -26,7 +26,9 @@ public class Solution {
 
 OPTIMAL SOLUTION
 
-    FLOYD'S CYCLE DETECTION
+    ALGORITHM USED - FLOYD'S CYCLE DETECTION
+
+    TECHNIQUE USED - FAST AND SLOW APPROACH
 
     /**
  * Definition for singly-linked list.
