9dd88a4e3892d90fcaaab0efa4be6499831b5c34
Aayush Kumar <254232086+aayushk2107@users.noreply.github.com>
2026-07-31 13:57:14 +0530

    Add palindrome check for linked list
    Implemented a two-pointer approach to check if a linked list is a palindrome with O(1) extra space by reversing the second half of the list.

--- 07-LinkedList/LC8_PalindromeLinkedList.java
+++ 07-LinkedList/LC8_PalindromeLinkedList.java
@@ -31,3 +31,55 @@ class Solution {
         return true;
     }
 }
+
+APPROACH -2 WITH O(1) EXTRA SPACE REMOVING OTHER LIST 
+
+    A -> B -> C -> null
+          ↑
+          |
+D --------+  THE LIST AFTER REVERSING
+
+
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast != null && fast.next != null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        if(fast==null){
+            slow=slow;
+        }
+        else{
+            slow=slow.next;
+        }
+        ListNode prev=null;
+        ListNode next=slow;
+        while(next != null){
+            next=next.next;
+            slow.next=prev;
+            prev=slow;
+            slow=next;
+        }
+        while(prev != null && head.next!=null){
+            if(head.val != prev.val){
+                return false;
+            }
+            head=head.next;
+            prev=prev.next;
+        }
+        return true;
+    }
+}
+    
