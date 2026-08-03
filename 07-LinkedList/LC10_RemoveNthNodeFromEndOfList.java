7d2ddf4a2dcd27a89233bd92ab863e3abb166c4e
Aayush Kumar <254232086+aayushk2107@users.noreply.github.com>
2026-08-03 07:14:49 +0530

    Update LC10_RemoveNthNodeFromEndOfList.java

--- 07-LinkedList/LC10_RemoveNthNodeFromEndOfList.java
+++ 07-LinkedList/LC10_RemoveNthNodeFromEndOfList.java
@@ -37,3 +37,41 @@ class Solution {
         return head;
     }
 }
+
+
+APPROACH 2 - SOLVED THIS IN ONE PASS USING TWO POINTERS FAST SLOW METHOD IT'S AN AMAZING SOLUTION TBH LOVED IT 
+
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head.next == null){
+            return null;
+        }
+        ListNode fast=head;
+        ListNode slow=head;
+        ListNode prev=null;
+        int indices=0;
+        while(fast != null){
+            indices++;
+            fast=fast.next;
+            if(indices > n){
+                prev=slow;
+                slow=slow.next;
+            }
+        }
+        if(slow == head){
+            return slow.next;
+        }
+        prev.next=slow.next;
+        return head;
+    }
+}
