fd7468675210cd18debe79a9d026a276d8440b61
Aayush kumar <aayushk2107@users.noreply.github.com>
2026-08-20 21:51:10 +0530

    Solved this one

---
+++ 02-TwoDArray/LC9_SpecialPositionsInABinaryMatrix.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numSpecial(int[][] mat) {
+        int specialPosition = 0;
+        for(int i=0;i<mat.length;i++){
+            int  col= 0;
+            int oneCount = 0;
+            for(int j=0;j<mat[i].length;j++){
+                if(mat[i][j] == 1){
+                    oneCount++;
+                    col = j;
+                }
+            }
+            if(oneCount == 1){
+                int onCount = 0;
+                for(int p = 0;p < mat.length;p++){
+                    if(mat[p][col] == 1){
+                        onCount++;
+                    }
+                }
+                if(onCount == 1){
+                    specialPosition++;
+                }
+            }
+        }
+        return specialPosition;
+    }
+}
