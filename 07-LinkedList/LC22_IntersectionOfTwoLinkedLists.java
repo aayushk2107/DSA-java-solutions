01319f39838221bfece6961409535e272e9e2b7b
Aayush Kumar <254232086+aayushk2107@users.noreply.github.com>
2026-08-27 19:28:05 +0530

    Implement second approach for intersection detection
    Added a second approach for finding the intersection of two linked lists using pointer traversal.

--- 07-LinkedList/LC22_IntersectionOfTwoLinkedLists.java
+++ 07-LinkedList/LC22_IntersectionOfTwoLinkedLists.java
@@ -1,4 +1,5 @@
-APPROACH 1
+APPROACH 1 - STORE THE LIST IN THE HASHSET AND RUN THE LOOP AGAIN IF THAT REFERENCE WE FOUND IN THE LIST WHILE TRAVERSING IN THE SECOND
+    LIST IT MEANS INTERSECTION EXISTS
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -31,5 +32,30 @@ public class Solution {
             curr2 = curr2.next;
         }
         return ans;
+    }
+}
+
+APPROACH 2 
+    THE APPROACH FUNCTIONS IN THIS WAY - WHEN P REACHES NULL POINT IT TOWARDS HEADA AND WHEN Q REACHES NULL POINT IT TO HEADB SIMPLY JUST 
+
+    public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p = headA;
+        ListNode q = headB;
+        while(p != q){
+            if(p == null){
+                p = headB;
+            }
+            else{
+                p = p.next;
+            }
+            if(q == null){
+                q = headA;
+            }
+            else{
+                q = q.next;
+            }
+        }
+        return p;
     }
 }
