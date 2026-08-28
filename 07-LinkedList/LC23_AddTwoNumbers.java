96542d6f3ee4f021aa5850d6adeb7a6c23686743
Aayush Kumar <254232086+aayushk2107@users.noreply.github.com>
2026-08-28 13:10:01 +0530

    Update LC23_AddTwoNumbers.java

--- 07-LinkedList/LC23_AddTwoNumbers.java
+++ 07-LinkedList/LC23_AddTwoNumbers.java
@@ -8,39 +8,42 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int carry = 0;
         ListNode dummy = new ListNode(0);
         ListNode tail = dummy;
         while(l1 != null || l2 != null){
-            if(l1 != null && l2 != null){
-                int digit = l1.val + l2.val + carry;
-                carry = digit / 10;
-                int value = digit % 10;
-                ListNode node = new ListNode(value);
-                tail.next = node;
-                tail = tail.next;
-                l1 = l1.next;
-                l2 = l2.next;
+            int x = 0;
+            int y = 0;
+            if(l1 != null){
+                x = l1.val;
             }
-            else if(l1 == null && l2 != null){
-                int digit = l2.val + carry;
-                carry = digit / 10;
-                int value = digit % 10;
-                ListNode node = new ListNode(value);
-                tail.next = node;
-                tail = tail.next;
-                l2 = l2.next;
+            if(l2 != null){
+                y = l2.val;
             }
-            else if(l1 != null && l2 == null){
-                int digit = l1.val + carry;
-                carry = digit / 10;
-                int value = digit % 10;
-                ListNode node = new ListNode(value);
-                tail.next = node;
-                tail = tail.next;
+            int digit = x + y + carry;
+            carry = digit / 10;
+            int value = digit % 10;
+            ListNode node = new ListNode(value);
+            tail.next = node;
+            tail = tail.next;
+
+            if(l1 != null){
                 l1 = l1.next;
+            }
+            if(l2 != null){
+                l2 = l2.next;
             }
         }
         if(carry != 0){
