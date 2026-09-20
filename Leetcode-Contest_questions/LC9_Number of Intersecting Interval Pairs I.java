07411c9b123fdccc32730f9d2f95f72068410f42
Aayush kumar <aayushk2107@users.noreply.github.com>
2026-09-20 19:31:51 +0530

    a good time solving this and i will return to it agaain

---
+++ Leetcode-Contest_questions/LC9_Number of Intersecting Interval Pairs I.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countIntersectingIntervals(int[][] intervals) {
+        int countintersections = 0;
+        for(int i = 0;i < intervals.length;i++){
+            for(int j = i + 1;j < intervals.length;j++){
+                if(intervals[i][0] <= intervals[j][1] && intervals[i][1] >= intervals[j][0]){
+                    countintersections++;
+                }
+            }
+        }
+        return countintersections;
+    }
+}
